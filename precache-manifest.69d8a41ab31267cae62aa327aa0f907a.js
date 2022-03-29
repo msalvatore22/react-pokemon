4da092a34204caf1920be950e18895350119b14c
Michael Salvatore <msalvatore22@gmail.com>
2022-03-29 10:36:07 -0400

    Updates

---
+++ precache-manifest.69d8a41ab31267cae62aa327aa0f907a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5af8c4cdada3d9b5c832",
+    "url": "/react-pokemon/static/css/main.10129c84.chunk.css"
+  },
+  {
+    "revision": "5af8c4cdada3d9b5c832",
+    "url": "/react-pokemon/static/js/main.5af8c4cd.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/react-pokemon/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "b75323fd636df612d81b",
+    "url": "/react-pokemon/static/js/2.b75323fd.chunk.js"
+  },
+  {
+    "revision": "60cd7734b32fb30a8db3",
+    "url": "/react-pokemon/static/js/runtime~main.60cd7734.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-pokemon/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b1b5379bcfbfcaad5653d8eba865fe41",
+    "url": "/react-pokemon/index.html"
+  }
+];
